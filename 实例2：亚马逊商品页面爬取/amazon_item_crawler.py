cbcc90cb0f11f9d7754ef5607afa3bc71461fd04
starryZhou <904260897@qq.com>
2018-08-12 14:56:48 +0800

    补充亚马逊商品页面爬取基本代码

---
+++ 实例2：亚马逊商品页面爬取/amazon_item_crawler.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import requests
+
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        print(r.text)
+    except BaseException:
+        return "fail "
+
+
+def main():
+    url = "http://amazon.cn/gp/product/B01M8L5Z3Y"
+    getHTMLText(url)
+
+
+if __name__ == '__main__':
+    main()
