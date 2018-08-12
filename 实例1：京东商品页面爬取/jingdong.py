31e48506ab5b08d49a60dc8a69ffee774109ed23
starryZhou <904260897@qq.com>
2018-08-12 14:43:15 +0800

    更改为函数式

--- 实例1：京东商品页面爬取/jingdong.py
+++ 实例1：京东商品页面爬取/jingdong.py
@@ -1,9 +1,20 @@
 import requests
-url = "http://item.jd.com/7834050.html"
-try:
-    r = requests.get(url)
-    r.raise_for_status()
-    r.encoding = r.apparent_encoding
-    print(r.text)
-except:
-    print("fail")
+
+def getHTMLText(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        print(r.text)
+    except:
+        print("fail")
+
+
+def main():
+    url = "http://item.jd.com/7834050.html"
+    getHTMLText(url)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
