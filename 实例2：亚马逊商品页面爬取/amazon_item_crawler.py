33173b11b50968eb73983c9642686fb83cd9aa76
starryZhou <904260897@qq.com>
2018-08-12 15:11:00 +0800

    亚马逊

--- 实例2：亚马逊商品页面爬取/amazon_item_crawler.py
+++ 实例2：亚马逊商品页面爬取/amazon_item_crawler.py
@@ -6,7 +6,8 @@ import requests
 
 def getHTMLText(url):
     try:
-        r = requests.get(url)
+        #kv={'user-agent':'Mozilla/5.0'}
+        r=requests.get(url)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         print(r.text)
