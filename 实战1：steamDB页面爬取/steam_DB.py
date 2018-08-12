c7fb932a355929b4c24d47be35eca36db94fdf17
starryZhou <904260897@qq.com>
2018-08-12 20:04:27 +0800

    完成实战1，steamDB网站的网页爬取

--- 实战1：steamDB页面爬取/steam_DB.py
+++ 实战1：steamDB页面爬取/steam_DB.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
+# steamDatabase的地址：https://steamdb.info/
+
+import requests
 
 
+def getDB(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        print(r.text)
+    except BaseException:
+        print("fail")
+
+
+def main():
+    url = "https://steamdb.info/"
+    getDB(url)
+
+
+if __name__ == '__main__':
+    main()
